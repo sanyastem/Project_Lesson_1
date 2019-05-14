c226aa484d2aba7f8bafa9cf7fa94cb189205a59
IrinaKorteleva <irkakort@gmail.com>
2019-05-14 13:37:09 -0400

    add loginIn metod

--- BL/Interfaces/ILoginService.cs
+++ BL/Interfaces/ILoginService.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BL.Models;
 
 namespace BL.Interfaces
 {
     interface ILoginService
     {
-        void LogIn();
+        void LogIn(List<User> user, User userForCheck);
 
         void LogOut();
     }
--- BL/Services/LoginService.cs
+++ BL/Services/LoginService.cs
@@ -1,17 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BL.Interfaces;
+using BL.Models;
 
 namespace BL.Services
 {
     public class LoginService : ILoginService
     {
-        public void LogIn()
+        public void LogIn(List<User> user, User userForCheck)
         {
-            throw new NotImplementedException();
+            if (user.Count == 0||string.IsNullOrEmpty(userForCheck.Login)||string.IsNullOrEmpty(userForCheck.Password))
+            {
+                userForCheck.CheckLogin = false;
+            }
+
+            User userFirst = user.FirstOrDefault(row => userForCheck.Login == row.Login && userForCheck.Password == row.Password);
+            if (userFirst!=null)
+            {
+                userForCheck.CheckLogin = true;
+                return;
+            }
+
+            userForCheck.CheckLogin = false;
         }
 
         public void LogOut()
